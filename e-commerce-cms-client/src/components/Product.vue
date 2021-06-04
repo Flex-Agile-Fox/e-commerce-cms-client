dc7834a15eb69b588fdbf2e19c52143a9ac6ca08
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-06-04 23:19:21 +0800

    Vue Init, Added Product Component in Products.vue and create product.vue in component

---
+++ e-commerce-cms-client/src/components/Product.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="product.image_url" class="card-img-top" :alt="product.name" />
+    <div class="card-body">
+      <h5 class="card-title">{{ product.name }}</h5>
+      <span class="badge bg-secondary">{{ product.category }}</span>
+      <p class="card-text">
+        {{ product.description }}
+      </p>
+      <p class="card-text">Rp. {{ product.price }}</p>
+      <p class="card-text">Stock: {{ product.stock }}</p>
+    </div>
+    <div class="card-body">
+      <button class="btn btn-primary">Edit</button>
+      <button class="btn btn-danger">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: {
+    product: Object,
+  },
+};
+</script>
+<style></style>
---
+++ e-commerce-cms-client/src/views/Products.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="product">
+    <Product v-for="product in products" :product="product" :key="product.id" />
+  </div>
+</template>
+
+<script>
+import Product from "../components/Product.vue";
+
+export default {
+  name: "Products",
+  components: {
+    Product,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 2,
+          name: "test to update",
+          image_url:
+            "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+          price: 1000,
+          stock: 5,
+          category: "Hobby",
+          description: "Limited Edition",
+          createdAt: "2021-06-04T11:47:05.864Z",
+          updatedAt: "2021-06-04T11:47:05.864Z",
+        },
+        {
+          id: 3,
+          name: "test to update",
+          image_url:
+            "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+          price: 1000,
+          stock: 5,
+          category: "Hobby",
+          description: "Limited Edition",
+          createdAt: "2021-06-04T11:47:06.755Z",
+          updatedAt: "2021-06-04T11:47:06.755Z",
+        },
+        {
+          id: 4,
+          name: "test to update",
+          image_url:
+            "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+          price: 1000,
+          stock: 5,
+          category: "Hobby",
+          description: "Limited Edition",
+          createdAt: "2021-06-04T11:47:07.685Z",
+          updatedAt: "2021-06-04T11:47:07.685Z",
+        },
+      ],
+    };
+  },
+};
+</script>
